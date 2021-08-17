f7a13c772f6de54aeed54e750b9951417f6634e0
SeppPenner <HansM2013@gmx.de>
2021-08-17 19:21:56 +0200

    Initial version.

---
+++ Changelog.md
@@ -0,0 +1,3 @@
+# Change history
+
+* **Version 1.0.0.0 (2021-08-17)** : 1.0 release.
---
+++ src/CheckLoggingBug/Program.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Hämmer Electronics">
+//   Copyright (c) All rights reserved.
+// </copyright>
+// <summary>
+//   The main program for the application.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CheckLoggingBug
+{
+    using System;
+
+    using Serilog;
+    using Serilog.Debugging;
+
+    /// <summary>
+    ///     The main program for the application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        public static void Main()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            SelfLog.Enable(Console.WriteLine);
+            Log.Warning("whatever contains {}");
+        }
+    }
+}
